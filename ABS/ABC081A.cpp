2e91ee29a4e9131b0a1edb163886011c3d0ef546
Iwamoto Naoya <august.third.naoya@gmail.com>
2021-11-14 12:21:56 +0900

    [add] AtCoder Beginners Selection
    C++で書き直し

---
+++ ABS/ABC081A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int count = 0;
+
+    for (char c : s) {
+        if (c == '1') {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
